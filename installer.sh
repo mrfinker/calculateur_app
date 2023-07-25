5b79515d7a3d8cbe8ceb708c13bef17cfc4c27da
Caleb Kiangebeni <101873757+mrfinker@users.noreply.github.com>
2023-07-25 11:05:50 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ mkdir "$download_folder"
 
 choix=$(zenity --list --title "Menu principal" --text "Choisissez une option :" --radiolist --column "Sélection" --column "Option" FALSE "Installer tous les paquets" FALSE "Sélectionner les paquets à installer" FALSE "Quitter le programme" --width 400 --height 250)
 
-path_distant="https://github.com/mrfinker/calculateur_app"
+path_distant="https://github.com/mrfinker/calculateur_app/"
 # Utiliser le dossier de téléchargement comme chemin d'enregistrement
 path_local="$download_folder/"
 user="$USER"
@@ -28,24 +28,30 @@ function dependance {
 # Fonction pour récupérer la liste des applications
 function liste {
     # TODO: Remplacez la commande ci-dessous par la liste de vos applications sur GitHub
-    apps=$(git ls-remote --heads https://github.com/mrfinker/calculateur_app.git | sed 's?.*refs/heads/??')
+    # apps=$(git ls-remote --heads https://github.com/votre_compte/votre_repo.git | sed 's?.*refs/heads/??')
 
     # Exemple de commande pour récupérer la liste des fichiers/dossiers dans votre repo GitHub
     apps=("google_chrome" "visual_studio_code")
 
     # Afficher la liste dans une boîte de dialogue Zenity
-    zenity --list \
+    selected_packages=$(zenity --list \
         --title "Liste des applications" \
         --text "Voici la liste de tous nos paquets à installer" \
         --width 400 \
         --height 800 \
         --ok-label "Suivant >" \
         --column "Nom" ${apps[*]}
+    )
+
+    if [ -z "$selected_packages" ]; then
+        echo "Aucun paquet sélectionné."
+        exit 1
+    fi
 }
 
 if [ "$choix" = "Installer tous les paquets" ]; then
     # Stocker le mot de passe de la machine utilisateur
-    read -s -p $'\n'"veuiller saisir votre mot de passe : " user_password
+    read -s -p $'\n'"veuillez saisir votre mot de passe : " user_password
     echo $''
 
     zenity --info --text "Téléchargement des fichiers depuis GitHub..." --width 400 --height 100
@@ -68,29 +74,14 @@ if [ "$choix" = "Installer tous les paquets" ]; then
 
 elif [ "$choix" = "Sélectionner les paquets à installer" ]; then
     # Stocker le mot de passe de la machine utilisateur
-    read -s -p $'\n'"veuiller saisir votre mot de passe : " user_password
+    read -s -p $'\n'"veuillez saisir votre mot de passe : " user_password
     echo $''
 
     liste
     
     zenity --info --text "Accès à la base de données\n(Téléchargement des fichiers depuis GitHub...)" --width 400 --height 100
 
-    selected_packages=$(zenity --list --checklist --width 400 --height 400 \
-        --column "" --column "Paquet" \
-        FALSE "google_chrome" \
-        FALSE "visual_studio_code" 
-        #FALSE "app3" 
-        # Ajoutez autant de lignes que nécessaire pour chaque paquet
-    )
-
-    if [ -z "$selected_packages" ]; then
-        echo "Aucun paquet sélectionné."
-        exit 1
-    fi
-
-    zenity --question --text "Confirmez-vous l'installation des paquets suivants ?" --width 400 --height 200
-
-    zenity --info --text "Téléchargement des fichiers depuis GitHub..." --width 400 --height 100
+    zenity --question --text "Téléchargement des fichiers depuis GitHub...\nConfirmez-vous l'installation des paquets suivants ?" --width 400 --height 200
 
     # Télécharger les fichiers sélectionnés depuis GitHub en utilisant git clone
     git clone "$path_distant" "$path_local"
@@ -98,9 +89,14 @@ elif [ "$choix" = "Sélectionner les paquets à installer" ]; then
     echo "Installation en cours (Mot de passe utilisateur local requis) !!!"
 
     for package in $selected_packages; do
-        if ls "$path_local$package"*.deb >/dev/null 2>&1; then
-            echo "$user_password" | sudo -S dpkg -i "$path_local$package"*.deb
-            echo "$user_password" | sudo -S rm "$path_local$package"*.deb
+        if ls "$path_local/$package"*.deb >/dev/null 2>&1; then
+            # Vérifier et installer les dépendances avant d'installer le paquet
+            echo "$user_password" | sudo -S dpkg -i "$path_local/$package"*.deb
+            if [ $? -ne 0 ]; then
+                echo "$user_password" | sudo -S apt-get -f install -y
+                echo "$user_password" | sudo -S dpkg -i "$path_local/$package"*.deb
+            fi
+            echo "$user_password" | sudo -S rm "$path_local/$package"*.deb
         fi
     done
 
